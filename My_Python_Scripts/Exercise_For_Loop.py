e7a00093f707d876ab8f6984189dae08e77b56ad
Ashmi Chheda <31774401+ashmichheda@users.noreply.github.com>
2019-01-11 22:56:24 +0530

    added python files

---
+++ My_Python_Scripts/Exercise_For_Loop.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 11 22:36:43 2019
+
+@author: Sanjay
+"""
+
+for n in range(2, 11, 2):
+    print(n)
+print("Goodbye!")
+
+print("Hello")
+for n in range(10, 0, -2):
+    print(n)
+
+i = 0    
+for a in range(1, 7):
+    i += a
+print(i)
+

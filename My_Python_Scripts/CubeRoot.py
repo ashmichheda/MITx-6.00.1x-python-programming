13261a678ea5efae5d584aa1e41465b1711795b7
Ashmi Chheda <31774401+ashmichheda@users.noreply.github.com>
2019-01-15 22:52:22 +0530

    Iteration algorithms examples

---
+++ My_Python_Scripts/CubeRoot.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 15 22:13:41 2019
+
+@author: Ashmi
+"""
+"""
+Check whether number is perfect cube or not
+Use of absolute function
+"""
+num = int(input("Enter a number: "))
+ans = 0
+while ans**3 < abs(num):
+    ans = ans + 1
+
+if ans** 3 != abs(num):
+    print(str(num) +" is not a perfect cube")
+else:
+    if num < 0:
+        ans = -ans
+    print('Cube root of '+ str(num) +" is " + str(ans))

b7a4afdb088013d553aca8b9e0cdf8bbfc7615ca
Ashmi Chheda <31774401+ashmichheda@users.noreply.github.com>
2019-01-17 18:33:27 +0530

    Added comments

--- Week-1 Python-Basics/Problem_1.py
+++ Week-1 Python-Basics/Problem_1.py
@@ -1,21 +1,19 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Jan 15 22:55:43 2019
-
 @author: Ashmi
 """
-"""
-Problem set 1
-Problem 1
 
-Assume s is a string of lower case characters.
+# Problem set 1
+# Problem 1
 
-Write a program that counts up the number of vowels contained 
-in the string s. 
-Valid vowels are: 'a', 'e', 'i', 'o', and 'u'. 
-For example, if s = 'azcbobobegghakl', your program should print:
-Number of vowels: 5
-"""
+# Assume s is a string of lower case characters.
+
+# Write a program that counts up the number of vowels contained 
+# in the string s. 
+# Valid vowels are: 'a', 'e', 'i', 'o', and 'u'. 
+# For example, if s = 'azcbobobegghakl', your program should print:
+# Number of vowels: 5
 
 s = 'aeiouash'
 countVowel = 0

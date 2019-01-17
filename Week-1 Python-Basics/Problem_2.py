38f7466bb9f87fe3319b4c1c6dba09234d5f2355
Ashmi Chheda <31774401+ashmichheda@users.noreply.github.com>
2019-01-17 18:34:10 +0530

    Added comments

--- Week-1 Python-Basics/Problem_2.py
+++ Week-1 Python-Basics/Problem_2.py
@@ -1,20 +1,14 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Jan 15 23:04:07 2019
-
 @author: Ashmi
 """
 
-"""
-Problem 2
-
-Assume s is a string of lower case characters.
-
-Write a program that prints the number of times the string 'bob' occurs in s. 
-For example, if s = 'azcbobobegghakl', then your program should print
-
-Number of times bob occurs is: 2
-"""
+# Problem 2
+# Assume s is a string of lower case characters.
+# Write a program that prints the number of times the string 'bob' occurs in s. 
+# For example, if s = 'azcbobobegghakl', then your program should print
+# Number of times bob occurs is: 2
 
 s = 'azcbobobegghakl'
 countSubstring = 0

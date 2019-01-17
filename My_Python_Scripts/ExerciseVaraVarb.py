f09436228467674ed19877870ed1118602eaf4db
Ashmi Chheda <31774401+ashmichheda@users.noreply.github.com>
2019-01-17 18:31:45 +0530

    Added relevant comments

--- My_Python_Scripts/ExerciseVaraVarb.py
+++ My_Python_Scripts/ExerciseVaraVarb.py
@@ -5,22 +5,16 @@ Created on Fri Jan 11 22:06:44 2019
 @author: Ashmi
 """
 
+# Write a piece of Python code that evaluates varA and varB, 
+# and then prints out one of the following messages:
+# "string involved" if either varA or varB are strings
+# "bigger" if varA is larger than varB
+# "equal" if varA is equal to varB
+# "smaller" if varA is smaller than varB
+
 varA = "as"
 varB = "niyati"
 
-"""
-Write a piece of Python code that evaluates varA and varB, 
-and then prints out one of the following messages:
-
-"string involved" if either varA or varB are strings
-
-"bigger" if varA is larger than varB
-
-"equal" if varA is equal to varB
-
-"smaller" if varA is smaller than varB
-
-"""
 if type(varA) == str or type(varB) == str:
     print("string involved")
     

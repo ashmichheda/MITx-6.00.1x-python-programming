eeaa4fc5d6f3046340382b583f32413da902e74d
Ashmi Chheda <31774401+ashmichheda@users.noreply.github.com>
2019-01-18 23:02:41 +0530

    Week 2: Simple program samples

---
+++ Week 2-Simple Programs/cheer.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 18 22:53:43 2019
+
+@author: Ashmi
+"""
+an_letters = "aefhilmnorsxAEFHILLMNORSX"
+
+word = input("I will cheer for you! Enter a word: ")
+times = int(input("Enthusiasm level (1 - 10): "))
+i = 0;
+
+while i < len(word):
+    char = word[i]
+    if(char in an_letters):
+        print("Give me an "+ char + "! "+ char)
+    else:
+        print("Give me a "+ char + "! "+ char)
+    i += 1
+print("What does that spell!? ")
+for i in range(times):
+    print(word, "!!!")

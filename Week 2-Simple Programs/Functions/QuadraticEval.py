378d8fa4a86dde3a5f965769ebb549f91e727199
Ashmi Chheda <31774401+ashmichheda@users.noreply.github.com>
2019-02-17 19:03:14 +0530

    Quadratic Eval problem

---
+++ Week 2-Simple Programs/Functions/QuadraticEval.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 17 18:59:34 2019
+
+@author: Ashmi
+
+
+Write to Python function evalQuadratic(a, b, c, x),, 
+that returns the value of the quadratic 
+[mathjaxinline] to \ cdot x ^ 2 + b \ cdot x + c [/ mathjaxinline].
+This function takes in four numbers and returns to single number.
+
+"""
+def evalQuadratic(a, b, c, x):
+    '''
+    a, b, c: numerical values for the coefficients of a quadratic equation
+    x: numerical value at which to evaluate the quadratic.
+    '''
+    finalVal = a*(x*x) + (b*x) + c
+    return finalVal
+
+#print(evalQuadratic(1, 1, 0, 2))

e59e54b5ab27f66e1ad7317f7cb2a91c81a73c26
Ashmi Chheda <31774401+ashmichheda@users.noreply.github.com>
2019-02-17 18:58:19 +0530

    Square problem

---
+++ Week 2-Simple Programs/Functions/Square.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 17 18:55:09 2019
+
+@author: Ashmi
+
+Write to Python function square,
+that takes in one number and returns the square of that number.
+
+This function takes in one number and returns one number.
+
+"""
+
+def square(x):
+    '''
+    x: int or float
+    '''
+    return x*x
+
+#print(square(6.0))

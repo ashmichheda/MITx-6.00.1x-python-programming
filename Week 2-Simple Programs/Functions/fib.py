75ccbcd9b44ff17c149b879c17c5c437dd47dfec
Ashmi Chheda <31774401+ashmichheda@users.noreply.github.com>
2019-06-11 22:47:58 +0530

    added fibonacci recursion problem

---
+++ Week 2-Simple Programs/Functions/fib.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 11 22:43:52 2019
+
+@author: Ashmi
+"""
+
+def fibonacci(x):
+    
+    if x == 0 or x == 1:
+        return 1
+    else:
+        return fibonacci(x-1) + fibonacci(x-2)
+
+result  = fibonacci(5)
+print("Fib total is: "+str(result))

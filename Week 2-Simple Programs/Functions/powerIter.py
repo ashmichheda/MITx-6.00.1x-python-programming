5603a34f805e0785d24cd332ee7fef65f93a1a6b
Ashmi Chheda <31774401+ashmichheda@users.noreply.github.com>
2019-06-12 22:31:58 +0530

    added comments

--- Week 2-Simple Programs/Functions/powerIter.py
+++ Week 2-Simple Programs/Functions/powerIter.py
@@ -7,6 +7,7 @@ Created on Tue Jun 11 21:52:56 2019
 
 def iterPower(base, exp):
     
+    # when exp is 0, result will be 1 by default
     result = 1
     while exp > 0:
         result = result * base
